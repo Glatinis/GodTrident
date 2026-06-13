17631fda3042bebd4254c24ecf4ba574946bbe07
Glatinis <63927852+Glatinis@users.noreply.github.com>
2026-06-13 20:41:24 +0800

    Update CritLightningAbility.java

--- src/main/java/com/github/Glatinis/godTrident/abilities/CritLightningAbility.java
+++ src/main/java/com/github/Glatinis/godTrident/abilities/CritLightningAbility.java
@@ -2,5 +2,5 @@ package com.github.Glatinis.godTrident.abilities;
 
 public final class CritLightningAbility implements Ability {
     @Override public String getName() { return "crit_lightning"; }
-    @Override public long getCooldownMillis() { return 1L; }
+    @Override public long getCooldownMillis() { return 10L; }
 }
